1736305b0de24ffbedde721337ca9a2a4a2097c2
Palash <palash.bhowmick@gmail.com>
2016-05-28 21:09:55 +0530

    react if obstacle detected

--- RoboCar.ino
+++ RoboCar.ino
@@ -2,10 +2,6 @@
 int motor_left[] = { 8, 9 };
 int motor_right[] = { 10, 11 };
 
-
-const int trigPin = 2;
-const int echoPin = 4;
-
 // --------------------------------------------------------------------------- Setup
 void setup() {
 	Serial.begin(9600);
@@ -15,95 +11,34 @@ void setup() {
 		pinMode(motor_left[i], OUTPUT);
 		pinMode(motor_right[i], OUTPUT);
 	}
-	motor_stop();
-	while (!Serial.available());
-	Serial.println("--- PALASH's ROBO CAR");
-	Serial.println("1) FORWARD");
-	Serial.println("2) BACKWARD");
-	Serial.println("3) TURN LEFT");
-	Serial.println("4) TURN WRITE");
-	Serial.println("0) STOP");
 }
 
-boolean obstacle = false;
+const int trigPin = 12;
+const int echoPin = 2;
+
+const int threshold = 8;// 8 inches
+
+byte prevStep = 0; // 0-stop,1-frd,2-bck,3-lt,4-rt
+
 // --------------------------------------------------------------------------- Loop
 void loop() {
 	int inches = getDistanceInInches();
-	if (inches < 8) {
-		obstacle = true;
+	Serial.println(inches);
+	if (inches <= threshold) {
+		// obstacle ahead - must react
+		motor_stop();
+		delay(250);
+		down();
+		delay(500);
+		right();
+		delay(500);
+		left();
+		delay(500);
 	}
-	if (Serial.available()) {
-		Serial.print(">");
-		char c = Serial.read();
-		if (c == '0') {
-			Serial.println("stop");
-			motor_stop();
-		}
-		else if (c == '3') {
-			if (obstacle) {
-				Serial.println("can't go");
-			}
-			else {
-				drive_forward();
-			}
-		}
-		else if (c == '4') {
-			Serial.println("back");
-			drive_backward();
-		}
-		else if (c == '1') {
-			Serial.println("left");
-			turn_left();
-		}
-		else if (c == '2') {
-			Serial.println("right");
-			turn_right();
-		}
+	else {
+		up();
 	}
 	delay(10);
-}
-
-// --------------------------------------------------------------------------- Drive
-
-void motor_stop() {
-	digitalWrite(motor_left[0], LOW);
-	digitalWrite(motor_left[1], LOW);
-
-	digitalWrite(motor_right[0], LOW);
-	digitalWrite(motor_right[1], LOW);
-	delay(25);
-}
-
-void drive_forward() {
-	digitalWrite(motor_left[0], HIGH);
-	digitalWrite(motor_left[1], LOW);
-
-	digitalWrite(motor_right[0], HIGH);
-	digitalWrite(motor_right[1], LOW);
-}
-
-void drive_backward() {
-	digitalWrite(motor_left[0], LOW);
-	digitalWrite(motor_left[1], HIGH);
-
-	digitalWrite(motor_right[0], LOW);
-	digitalWrite(motor_right[1], HIGH);
-}
-
-void turn_left() {
-	digitalWrite(motor_left[0], LOW);
-	digitalWrite(motor_left[1], HIGH);
-
-	digitalWrite(motor_right[0], HIGH);
-	digitalWrite(motor_right[1], LOW);
-}
-
-void turn_right() {
-	digitalWrite(motor_left[0], HIGH);
-	digitalWrite(motor_left[1], LOW);
-
-	digitalWrite(motor_right[0], LOW);
-	digitalWrite(motor_right[1], HIGH);
 }
 
 int getDistanceInInches() {
@@ -128,7 +63,51 @@ int getDistanceInInches() {
 
 	// convert the time into a distance
 	inches = microsecondsToInches(duration);
+	Serial.println(inches);
 	return inches;
+}
+
+// --------------------------------------------------------------------------- Drive
+
+void motor_stop() {
+	digitalWrite(motor_left[0], LOW);
+	digitalWrite(motor_left[1], LOW);
+
+	digitalWrite(motor_right[0], LOW);
+	digitalWrite(motor_right[1], LOW);
+	delay(25);
+}
+
+void left() {
+	digitalWrite(motor_left[0], HIGH);
+	digitalWrite(motor_left[1], LOW);
+
+	digitalWrite(motor_right[0], HIGH);
+	digitalWrite(motor_right[1], LOW);
+}
+
+void right() {
+	digitalWrite(motor_left[0], LOW);
+	digitalWrite(motor_left[1], HIGH);
+
+	digitalWrite(motor_right[0], LOW);
+	digitalWrite(motor_right[1], HIGH);
+}
+
+void up() {
+	digitalWrite(motor_left[0], LOW);
+	digitalWrite(motor_left[1], HIGH);
+
+	digitalWrite(motor_right[0], HIGH);
+	digitalWrite(motor_right[1], LOW);
+}
+
+void down() {
+	digitalWrite(motor_left[0], HIGH);
+	digitalWrite(motor_left[1], LOW);
+
+	digitalWrite(motor_right[0], LOW);
+	digitalWrite(motor_right[1], HIGH);
 }
 long microsecondsToInches(long microseconds)
 {
@@ -139,3 +118,4 @@ long microsecondsToInches(long microseconds)
 	// See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
 	return microseconds / 74 / 2;
 }
+
