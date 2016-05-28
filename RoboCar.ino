03a370f04cc4682d2ee65549de88d42df89d41b9
Palash <palash.bhowmick@gmail.com>
2016-05-28 18:48:05 +0530

    ultrasonic sensor added for obstacle detection

--- RoboCar.ino
+++ RoboCar.ino
@@ -21,34 +21,60 @@ void setup() {
 	Serial.println("0) STOP");
 }
 
+const int trigPin = 2;
+const int echoPin = 4;
 // --------------------------------------------------------------------------- Loop
 void loop() {
-
-	if (Serial.available()) {
-		Serial.print(">");
-		char c = Serial.read();
-		if (c == '0') {
-			Serial.println("stop");
-			motor_stop();
-		}
-		else if (c == '3') {
-			Serial.println("go");
-			drive_forward();
-		}
-		else if (c == '4') {
-			Serial.println("back");
-			drive_backward();
-		}
-		else if (c == '1') {
-			Serial.println("left");
-			turn_left();
-		}
-		else if (c == '2') {
-			Serial.println("right");
+	int inches = getDistanceInInches();
+	if (inches > 8) {
+		drive_forward();
+		delay(200);
+	}
+	else {
+		motor_stop();
+		delay(100);
+		drive_backward();
+		delay(250);
+		turn_left();
+		delay(2000);
+		inches = getDistanceInInches();
+		if (inches <= 8) {
 			turn_right();
+			delay(4000);
+			inches = getDistanceInInches();
+			if (inches <= 8) {
+				turn_left();
+				delay(2000);
+			}
 		}
 	}
-	delay(10);
+	delay(100);
+}
+
+int getDistanceInInches() {
+	// establish variables for duration of the ping,
+	// and the distance result in inches and centimeters:
+	long duration, inches, cm;
+
+	// The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+	// Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+	pinMode(trigPin, OUTPUT);
+	digitalWrite(trigPin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin, LOW);
+
+	// Read the signal from the sensor: a HIGH pulse whose
+	// duration is the time (in microseconds) from the sending
+	// of the ping to the reception of its echo off of an object.
+	pinMode(echoPin, INPUT);
+	duration = pulseIn(echoPin, HIGH);
+
+	// convert the time into a distance
+	inches = microsecondsToInches(duration);
+	Serial.println(inches);
+	return inches;
 }
 
 // --------------------------------------------------------------------------- Drive
@@ -93,3 +119,14 @@ void turn_right() {
 	digitalWrite(motor_right[0], LOW);
 	digitalWrite(motor_right[1], HIGH);
 }
+
+long microsecondsToInches(long microseconds)
+{
+	// According to Parallax's datasheet for the PING))), there are
+	// 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+	// second).  This gives the distance travelled by the ping, outbound
+	// and return, so we divide by 2 to get the distance of the obstacle.
+	// See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+	return microseconds / 74 / 2;
+}
+
