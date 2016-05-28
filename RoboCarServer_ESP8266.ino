3fe6d10aff6da7990282f43695eeee29705c6793
Palash <palash.bhowmick@gmail.com>
2016-05-28 21:45:01 +0530

    send json and proper http response

--- RoboCarServer_ESP8266.ino
+++ RoboCarServer_ESP8266.ino
@@ -3,16 +3,19 @@
 const char* ssid = "*****";
 const char* password = "*****";
 
-int ledPin = 2; // GPIO2
+void doResponse(WiFiClient &client, String json) {
+	client.println("HTTP/1.1 200 OK");
+	client.println("Content-Type: application/json");
+	client.println(""); //  do not forget this one
+	client.println(json);
+	client.stop();
+}
+
 WiFiServer server(80);
 
 void setup() {
 	Serial.begin(9600);
 	delay(10);
-
-
-	pinMode(ledPin, OUTPUT);
-	digitalWrite(ledPin, LOW);
 
 	// Connect to WiFi network
 	Serial.println();
@@ -50,7 +53,7 @@ void loop() {
 
 	// Wait until the client sends some data
 	Serial.println("new client");
-	while (!client.available()){
+	while (!client.available()) {
 		delay(1);
 	}
 
@@ -59,45 +62,18 @@ void loop() {
 	Serial.println(request);
 	client.flush();
 
-	// Match the request
-
-	int value = LOW;
-	if (request.indexOf("/LED=ON") != -1) {
-		digitalWrite(ledPin, HIGH);
-		value = HIGH;
-	}
-	if (request.indexOf("LED=OFF") != -1){
-		digitalWrite(ledPin, LOW);
-		value = LOW;
-	}
-
 	// Set ledPin according to the request
 	//digitalWrite(ledPin, value);
 
 
-	// Return the response
-	client.println("HTTP/1.1 200 OK");
-	client.println("Content-Type: text/html");
-	client.println(""); //  do not forget this one
-	client.println("<!DOCTYPE HTML>");
-	client.println("<html>");
-
-	client.print("Led pin is now: ");
-
-	if (value == HIGH) {
-		client.print("On");
-	}
-	else {
-		client.print("Off");
-	}
-	client.println("<br><br>");
-	client.println("Click <a href=\"/LED=ON\">here</a> turn the LED on pin 2 ON<br>");
-	client.println("Click <a href=\"/LED=OFF\">here</a> turn the LED on pin 2 OFF<br>");
-	client.println("</html>");
+	doResponse(client, "{\"status\":\"succsess\"}");
 
 	delay(1);
 	Serial.println("Client disonnected");
 	Serial.println("");
 
 }
+
+
+
 
